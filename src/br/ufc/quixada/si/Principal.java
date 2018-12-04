407efa1137bdba1182955dbd4deca62d94d21610
Marcus Vinicius <mv3265@gmail.com>
2018-12-04 16:50:59 -0300

    Adicionados testes.

--- src/br/ufc/quixada/si/Principal.java
+++ src/br/ufc/quixada/si/Principal.java
@@ -1,4 +1,19 @@
 package br.ufc.quixada.si;
 
 public class Principal {
+
+    public static void main(String[] args) {
+
+        ListaPessoas listaPessoas = new ListaPessoas();
+
+        Pessoa pessoa1 = new Cliente("João","233");
+        listaPessoas.adicionar(pessoa1);
+        Pessoa pessoa2 = new Gerente("Emson", "434");
+        listaPessoas.adicionar(pessoa2);
+        Pessoa pessoa3 = new Cliente("Jack", "3214");
+        listaPessoas.adicionar(pessoa3);
+
+        MenuInicial menuInicial = new MenuInicial();
+        menuInicial.mostrar();
+    }
 }
